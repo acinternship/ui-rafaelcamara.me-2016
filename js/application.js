ef491f0877ddda4abf638b5a7b6743318db6f45f
Rafael Câmara Magalhães <rafaelcamaram@gmail.com>
2016-07-22 09:24:02 -0300

    BackToTop Button

--- js/application.js
+++ js/application.js
@@ -1,6 +1,30 @@
 $(document).ready(function() {
     $('#skills').on('click', '.btnShowSkills', function() {
-       $('#skill-box').removeClass('hide');
-        $(this).parent().remove();
+        
+        var btnShowSkills = $(this);
+        
+        if(btnShowSkills.text() == 'SHOW MY SKILLS') {
+            btnShowSkills.text('HIDE MY SKILLS');    
+            btnShowSkills.closest('.container').find('#skill-box').fadeIn();
+        } else {
+            btnShowSkills.text('SHOW MY SKILLS');
+             btnShowSkills.closest('.container').find('#skill-box').fadeOut();
+        }
     });
+    
+    //Check to see if the window is top if not then display button
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 100) {
+			$('.scrollToTop').fadeIn();
+		} else {
+			$('.scrollToTop').fadeOut();
+		}
+	});
+	
+	//Click event to scroll to top
+	$('.scrollToTop').click(function(){
+		$('html, body').animate({scrollTop : 0},800);
+		return false;
+	});
+
 });
